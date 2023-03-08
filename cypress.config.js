43efc36037a4b248506abcb319ccab87c31c2b48
lorges <anthonypola100@gmail.com>
2023-03-08 15:22:22 +0100

    initialization commit

---
+++ cypress.config.js
@@ -0,0 +1,23 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    //defaultCommandTimeout: 10000,
+    videoUploadOnPasses: false,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      
+    },
+  },
+});
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+// import allureWriter from "@shelex/cypress-allure-plugin/writer";
+
+module.exports = defineConfig({
+    e2e: {
+        setupNodeEvents(on, config) {
+            allureWriter(on, config);
+            return config;
+        }
+    }
+});
